d413907bc372555335dcc79ab5c49f0d36e196a4
eclipse_user-andy <eclipse_andy@163.com>
2018-09-19 17:16:22 +0800

    修改了 TestDept

--- dept-interface/src/main/java/com/andy/dubbo/entity/TestDept.java
+++ dept-interface/src/main/java/com/andy/dubbo/entity/TestDept.java
@@ -3,6 +3,11 @@ package com.andy.dubbo.entity;
 public class TestDept {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 	}
 
 }
